840945592eabd689f936336904fd94512d430985
XallDaCheva <dachevaxall@gmail.com>
2025-10-26 16:39:43 +0700

    feat: add form modal checkup doctor

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\DoctorController;
 use Illuminate\Support\Facades\Route;
 
-
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/schedule', [DoctorController::class, 'index'])->name('schedule');
+Route::post('/doctor/schedule', [DoctorController::class, 'storeSchedule'])->name('doctor.schedule.store');
